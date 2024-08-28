3e22116ce3531e492368f9f15808d6e7a995c825
Ashish Toppo <ashishtoppo8958@gmail.com>
2024-08-29 01:57:41 +0530

    created the basic structure of the homepage without any css

--- app/views/home.php
+++ app/views/home.php
@@ -1,23 +1,22 @@
-<?php
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Carbon Footprint Calculator</title>
 </head>
 <body>
-    this is home page <br>
 
-    {{ $information }} <br>
+    <nav>
+        <ul>
+            <li><a href="">Sign Up</a></li>
+            <li><a href="">Sign In</a></li>
+        </ul>
+    </nav>
 
-    @for($i = 1; $i < 10; $i++)
-        @template
-            <b> $i </b> <br>
-        @endtemplate
-    @endfor
-
+    <div class="heading">
+        <h1>Carbon Footprint Calculator</h1>
+        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi voluptates omnis recusandae molestias quod et, labore sequi dolorum rerum maxime nihil voluptate veniam temporibus iste illo accusamus aspernatur itaque tenetur velit fugit quia laborum. Animi impedit nihil illo excepturi officiis ex eius corporis voluptas minus praesentium, sit sequi soluta nostrum.</span>
+    </div>
 </body>
 </html>
