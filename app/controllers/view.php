9eb20a1e73209d3e160be8862f5a81829850f136
Ashish Toppo <ashishtoppo8958@gmail.com>
2024-08-29 01:58:55 +0530

    created .htaccess file in the root of the project

--- app/controllers/view.php
+++ app/controllers/view.php
@@ -5,16 +5,7 @@ class view extends Controller {
 
 
     public function home ($data) {
-
-        $info = ['information' => 'this data is generated'];
-
-        $user = $this->model('user');
-
-        $data = $user->query("SELECT * FROM doctors WHERE 1");
-
-        echo "<pre>";
-
-        var_dump($data);
+        $info = [];
 
         $this->view('home', $info);
     }
