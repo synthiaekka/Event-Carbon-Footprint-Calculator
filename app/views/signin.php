46a59b5c4969a3148fefaea30a1681ee1cd5ae17
Ashish Toppo <ashishtoppo8958@gmail.com>
2024-08-29 02:04:43 +0530

    created signin and signup pages without css

---
+++ app/views/signin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign In - CarbonFootprint Calc</title>
+</head>
+<body>
+    <form action="">
+        <input type="email" placeholder="Enter your email">
+        <input type="password" placeholder="Enter your password">
+        <input type="submit">
+    </form>
+</body>
+</html>
---
+++ app/views/signup.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up - CarbonFootprint Calc</title>
+</head>
+<body>
+    <form action="">
+        <input type="text" placeholder="Enter your full name">
+        <input type="email" placeholder="Enter your email">
+        <input type="password" placeholder="Enter your password">
+        <input type="password" placeholder="Re-enter your password">
+        <select name="usertype" id="">
+            <option value="1">Event Organizer</option>
+            <option value="2">Participant</option>
+            <option value="3">Vendors</option>
+        </select>
+        <input type="submit">
+    </form>
+</body>
+</html>
