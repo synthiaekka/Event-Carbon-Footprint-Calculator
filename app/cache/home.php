bdc4905bf36afd2065afc616ed3fe5a59b2b842e
syntiaekka <synthiaekka04@gmail.com>
2024-08-29 10:58:25 +0530

    created css files

--- app/cache/home.php
+++ app/cache/home.php
@@ -3,20 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Carbon Footprint Calculator</title>
+    <title>Welcome | Event Carbon Footprint Calculator</title>
+    <link rel="stylesheet" href="./css/home.css">
 </head>
 <body>
-
-    <nav>
-        <ul>
-            <li><a href="">Sign Up</a></li>
-            <li><a href="">Sign In</a></li>
-        </ul>
-    </nav>
-
-    <div class="heading">
-        <h1>Carbon Footprint Calculator</h1>
-        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi voluptates omnis recusandae molestias quod et, labore sequi dolorum rerum maxime nihil voluptate veniam temporibus iste illo accusamus aspernatur itaque tenetur velit fugit quia laborum. Animi impedit nihil illo excepturi officiis ex eius corporis voluptas minus praesentium, sit sequi soluta nostrum.</span>
+    <div class="container">
+        <div class="character character-1"></div>
+        <div class="character character-2"></div>
+        <div class="character character-3"></div>
+        <div class="character character-4"></div>
+        <div class="content">
+            <h1 class="fade-in">Welcome to Event Carbon Footprint Calculator</h1>
+            <p class="fade-in">Organize sustainable events by tracking and reducing your carbon footprint.</p>
+            <div class="buttons">
+                <a href="signin.php" class="btn">Signin</a>
+                <a href="signup.php" class="btn">Signup</a>
+            </div>
+        </div>
     </div>
 </body>
 </html>
