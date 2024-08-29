02f3230cf5007a87f6b3b24e8ea7f991b2f351e6
Richa Choudhury <richachoudhury478@gmail.com>
2024-08-29 14:40:41 +0530

    created calculator1

---
+++ app/views/CALCULATOR1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/Calculator.css">
+    <title>College Event Carbon Footprint Calculator</title>
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+            <h1>Choose event type!!! </h1>
+            <div class="buttons">
+                <a href="college-occasion.php" class="btn">College Occasion</a>
+                <a href="events.html" class="btn">Wedding</a>
+                <a href="events.html" class="btn">Concerts</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
